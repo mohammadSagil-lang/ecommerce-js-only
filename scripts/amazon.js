ba97682c60d61be9703b24bb2289288e196c69a7
Sagil <rockynd397@gmail.com>
2025-06-18 05:58:23 +0530

    make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,10 @@
-const products=[{"image": "images/products/athletic-cotton-socks-6-pairs.jpg" 
-    ,"name":"Black and Gray Athletic Cotton Socks - 6 Pairs"
-    ,"rating":{
-        stars:4.5,
-        count:87
-    },
-    priceCents: 1090
-
-},{"image": "images/products/intermediate-composite-basketball.jpg" 
-    ,"name":"Intermediate Size Basketball"
-    ,"rating":{
-        stars:4,
-        count:127
-    },
-  priceCents: 2095},{"image": "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" 
-        ,"name":"Adults Plain Cotton T-Shirt - 2 Pack"
-        ,"rating":{
-            stars:4.5,
-            count:595
-        },
-      priceCents: 799}];
 let productsHTML=``;
 products.forEach((product) =>{
     const html=`
         <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src=${product["image"]}>
+              src="${product["image"]}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
@@ -66,10 +45,34 @@ products.forEach((product) =>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
-            Add to Cart
-          </button>
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product["id"]}" data-product-name="${product["name"]}" data-product-image="${product["image"]}" >Add to Cart</button>
         </div>`
   productsHTML+=html;
 });
 document.querySelector(".products-grid").innerHTML=productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button) =>{
+  button.addEventListener('click',() =>{
+    let matchingItem; 
+    cart.forEach((item,index)=>{
+      if(item.id==button.dataset.productId)
+      {
+        matchingItem=item;
+      }
+    })
+    if(matchingItem)
+    {
+      matchingItem.quantity++;
+    }
+    else
+    {
+      cart.push({
+        id: button.dataset.productId,
+        quantity: 1,
+        image: button.dataset.productImage,
+        name: button.dataset.productName
+      })
+    } 
+    console.log(cart);
+  })
+
+})
